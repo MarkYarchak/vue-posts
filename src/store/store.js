6cb45a9c3f2ac1105ee87b33f90f81c2c5e5ea0a
Mark <Mark Yarchak>
2019-03-22 10:35:26 +0200

    added vuex to localstorage

--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,23 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+// import VuexPersist from 'vuex-persist';
+import createPersistedState from 'vuex-persistedstate';
 import state from './state';
 import actions from './actions';
 import getters from './getters';
 import mutations from './mutations';
 
 Vue.use(Vuex);
+// const vuexPersist = new VuexPersist({
+//   key: 'my-app',
+//   storage: localStorage,
+// });
 
 export default new Vuex.Store({
   state,
   actions,
   getters,
   mutations,
+  // plugins: [vuexPersist.plugin],
+  plugins: [createPersistedState()],
 });
